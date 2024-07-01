bd8e6890e4f81926bef0c86aeed3b862b39de60d
Fahad Dhruba <dhruba.fahad2004@gmail.com>
2024-07-01 12:12:52 +0000

    chip

--- admin/engine/main.js
+++ admin/engine/main.js
@@ -64,7 +64,7 @@ onAuthStateChanged(auth, (user) => {
             const input = document.getElementById("boardRef");
             const filter = input.value.toUpperCase();
             const div = document.getElementById("reffilHolder");
-            const a = div.getElementsByTagName("button");
+            const a = div.getElementsByTagName("a");
             for (let i = 0; i < a.length; i++) {
               txtValue = a[i].textContent || a[i].innerText;
               if (txtValue.toUpperCase().indexOf(filter) > -1) {
